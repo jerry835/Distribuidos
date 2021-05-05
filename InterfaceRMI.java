a3b1d01d18a9a5b4cbc9219cae0b24807c0d823b
jerry835 <77866892+jerry835@users.noreply.github.com>
2021-05-05 17:02:44 -0500

    Add files via upload

---
+++ InterfaceRMI.java
@@ -0,0 +1,8 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface InterfaceRMI extends Remote
+{
+public float[][] multiplica_matrices(float[][] A,float[][] B, int N) throws RemoteException;
+
+}
