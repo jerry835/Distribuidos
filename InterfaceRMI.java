61d337c1e4f0d57e7045939d009c5bac0bb58fde
jerry835 <77866892+jerry835@users.noreply.github.com>
2021-05-05 17:00:49 -0500

    Delete InterfaceRMI.java

--- InterfaceRMI.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface InterfaceRMI extends Remote
-{
-public float[][] multiplica_matrices(float[][] A,float[][] B) throws RemoteException
-
-}
