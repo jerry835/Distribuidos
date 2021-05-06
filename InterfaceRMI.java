46155f5aa04a97f386ff668b967319f603498eb1
jerry835 <77866892+jerry835@users.noreply.github.com>
2021-05-05 21:29:08 -0500

    Update InterfaceRMI.java

--- InterfaceRMI.java
+++ InterfaceRMI.java
@@ -3,7 +3,6 @@ import java.rmi.RemoteException;
 
 public interface InterfaceRMI extends Remote
 {
-  public String mayusculas(String name) throws RemoteException;
-  public int suma(int a,int b) throws RemoteException;
-  public long checksum(int[][] m) throws RemoteException;
+public float[][] multiplica_matrices(float[][] A,float[][] B, int N) throws RemoteException;
+
 }
