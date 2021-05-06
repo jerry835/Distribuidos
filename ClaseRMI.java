e0f948e48be28079a29093abca437c0843d34398
jerry835 <77866892+jerry835@users.noreply.github.com>
2021-05-05 19:54:38 -0500

    Update ClaseRMI.java

--- ClaseRMI.java
+++ ClaseRMI.java
@@ -1,5 +1,6 @@
 import java.rmi.server.UnicastRemoteObject;
 import java.rmi.RemoteException;
+
 public class ClaseRMI extends UnicastRemoteObject implements InterfaceRMI
 {
   // es necesario que el contructor default de la clase ClaseRMI invoque el constructor de la super-clase
@@ -7,14 +8,20 @@ public class ClaseRMI extends UnicastRemoteObject implements InterfaceRMI
   {
     super( );
   }
-
-public float[][] multiplica_matrices(float[][] A,float[][] B,int N) throws RemoteException
-{
-  float[][] C = new float[N/2][N/2];
-  for (int i = 0; i < N/2; i++)
-    for (int j = 0; j < N/2; j++)
-      for (int k = 0; k < N; k++)
-        C[i][j] += A[i][k] * B[j][k];
-  return C;
-}
+  public String mayusculas(String s) throws RemoteException
+  {
+    return s.toUpperCase();
+  }
+  public int suma(int a,int b)
+  {
+    return a + b;
+  }
+  public long checksum(int[][] m) throws RemoteException
+  {
+    long s = 0;
+    for (int i = 0; i < m.length; i++)
+      for (int j = 0; j < m[0].length; j++)
+        s += m[i][j];
+    return s;
+  }
 }
