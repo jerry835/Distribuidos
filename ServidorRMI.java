e4f9dcca852f2053ad6be32c9f92cc6f3131b4c5
jerry835 <77866892+jerry835@users.noreply.github.com>
2021-05-05 19:49:56 -0500

    Update ServidorRMI.java

--- ServidorRMI.java
+++ ServidorRMI.java
@@ -4,9 +4,10 @@ public class ServidorRMI
 {
   public static void main(String[] args) throws Exception
   {
-    String url = "rmi://localhost/prueba";// url del puerto por defecto
-    ClaseRMI obj = new ClaseRMI();// instancia del objeto remoto
+    String url = "rmi://localhost/prueba";
+    ClaseRMI obj = new ClaseRMI();
 
-     Naming.rebind(url,obj);// registra la instancia en el rmiregistry
+    // registra la instancia en el rmiregistry
+    Naming.rebind(url,obj);
   }
 }
